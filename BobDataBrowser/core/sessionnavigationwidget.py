ed0d59fd959fee52bff4aaf14ebf943c492ccf00
Nicholas Cain <nicain.seattle@gmail.com>
2017-03-14 22:20:00 -0700

    same figue doesnt work becase of webgl

--- BobDataBrowser/core/sessionnavigationwidget.py
+++ BobDataBrowser/core/sessionnavigationwidget.py
@@ -2,7 +2,7 @@ from bokeh.plotting import Figure
 from BobDataBrowser.core.utilities import turn_off_axes_labels, color_dict
 import collections
 import allensdk.brain_observatory.stimulus_info as si
-from bokeh.models import CustomJS, ColumnDataSource, Slider, Span, PanTool, CrosshairTool, HoverTool
+from bokeh.models import CustomJS, ColumnDataSource, Slider, Span, PanTool, CrosshairTool, HoverTool, TapTool
 from bokeh.colors import RGB
 import sys
 
@@ -13,10 +13,8 @@ class SessionNavigationWidget(object):
         self.app = app
 
         ht = HoverTool(tooltips=None)
-        # ht.mode
-
-
         ct = CrosshairTool(dimensions='height')
+        tt = TapTool()
 
         # ct.
         self.figure = Figure(plot_height=70,
@@ -27,17 +25,6 @@ class SessionNavigationWidget(object):
         self.figure.xgrid.grid_line_color = None
         self.figure.ygrid.grid_line_color = None
         self.figure.toolbar.logo = None
-        # self.figure.toolbar.tools = [ht, ct]
-        # self.figure.toolbar.active_tap = None
-
-        # self.figure.toolbar_location = None
-        # for x in self.figure.toolbar.tools:
-        #     print x.active
-        #
-        # self.figure.toolbar.
-        #
-        # sys.exit()
-
 
     def initialize(self):
 
@@ -59,4 +46,11 @@ class SessionNavigationWidget(object):
             D['color'].append(RGB(*tuple(color_list)))
 
 
-        self.figure.quad(**D)
+        self.stimulus_epoch_renderer = self.figure.quad(**D)
+
+        taptool = self.figure.select(type=TapTool)
+        def update(attr, old, new):
+            print attr, old, new
+            # new_index = new['1d']['indices'][0]
+            # self.app.active_cell_manager.set_active_cell(new_index) #CAREFUL: new_index referes to row in ColumnDataSource, not cell_index
+        self.stimulus_epoch_renderer.data_source.on_change('selected', update)
